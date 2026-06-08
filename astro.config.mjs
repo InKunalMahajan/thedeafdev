241cfae24df5a41e8e8ed60e0ab4bd8647f3d053
Kunal Pundalik Mahajan <Inkunalmahajan@gmail.com>
2026-06-09 00:05:23 +0530

    git commit -m "Deploy website"

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://thedeafdev.in'
     vite: {
         plugins: [tailwindcss()],
     },
