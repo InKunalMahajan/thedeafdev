6e214cdfab082c360c94460f7b03dedad8d2d288
Kunal Pundalik Mahajan <Inkunalmahajan@gmail.com>
2026-06-09 00:41:45 +0530

    Remove site URL from astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://thedeafdev.in',
     vite: {
         plugins: [tailwindcss()],
     },
